11d1dd5fa034af853d8cb147d7534f176a19c7c8
Loonzie <selinbolamat@hotmail.com>
2025-03-29 18:23:12 +0300

    x
    y

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject StartScreen, SettingsScreen;
+    public GameObject Audio;
+    public AudioSource bgsound;
+    public Slider VolumeSlider; 
+void Start()
+    {
+        StartScreen.SetActive(true);
+        SettingsScreen.SetActive(false);
+        bgsound = Audio.GetComponent<AudioSource>();
+        bgsound.Play();
+        if (VolumeSlider != null && bgsound != null)
+        {
+            bgsound.volume = VolumeSlider.value;
+        }
+    }
+    public void OpenSettings()
+    {
+        StartScreen.SetActive(false);
+        SettingsScreen.SetActive(true);
+    }
+
+    public void BackToStart()
+    {
+        StartScreen.SetActive(true);
+        SettingsScreen.SetActive(false);
+    }
+
+    public void OnVolumeChanged()
+    {
+        if (bgsound != null)
+        {
+            bgsound.volume = VolumeSlider.value;
+        }
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
